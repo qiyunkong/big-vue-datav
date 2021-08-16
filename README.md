0d6ddb815720c329d3ce0b573209c157f39a3df0
qiyunkong <2572922513@qq.com>
2021-08-16 10:56:44 +0800

    init project

---
+++ README.md
@@ -0,0 +1,48 @@
+# big-vue-datav
+
+> A Vue.js project 初始化项目
+
+
+## 
+
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+
+# run unit tests
+npm run unit
+
+# run e2e tests
+npm run e2e
+
+# run all tests
+npm test
+```
+
+## 相关依赖
+
+``` bash
+#  http://datav.jiaminghi.com/
+npm install @jiaminghi/data-view
+npm install echarts
+npm install echarts-gl
+npm install vue-router
+```
+
+
+## 功能需求
+
+
+## 处理BUG
---
+++ src/main.js
@@ -0,0 +1,29 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import dataV from '@jiaminghi/data-view'
+import * as echarts from 'echarts'
+
+// 按需引入vue-awesome图标
+import Icon from 'vue-awesome/components/Icon'
+import 'vue-awesome/icons/chart-bar.js'
+import 'vue-awesome/icons/chart-area.js'
+import 'vue-awesome/icons/chart-pie.js'
+import 'vue-awesome/icons/chart-line.js'
+import 'vue-awesome/icons/align-left.js'
+
+Vue.config.productionTip = false
+Vue.prototype.$echarts = echarts
+
+Vue.use(dataV)
+Vue.component('icon', Icon)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
